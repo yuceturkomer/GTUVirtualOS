eccdd9fdbbd83ef2f64bebe582382f2675544c7b
hmenn <hmen.56@gmail.com>
2016-10-12 00:13:19 +0300

    gitignore fix2

--- moc_MailBox.cpp
+++ moc_MailBox.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MailBox.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'MailBox.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.2.1. It"
+#error "This file was generated using the moc from 5.5.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,18 +20,19 @@
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox_t {
     QByteArrayData data[1];
-    char stringdata[36];
+    char stringdata0[35];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData) \
+    qptrdiff(offsetof(qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
     )
 static const qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox_t qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox = {
     {
-QT_MOC_LITERAL(0, 0, 34)
+QT_MOC_LITERAL(0, 0, 34) // "GtuVirtualOperatingSystem::Ma..."
+
     },
-    "GtuVirtualOperatingSystem::MailBox\0"
+    "GtuVirtualOperatingSystem::MailBox"
 };
 #undef QT_MOC_LITERAL
 
@@ -61,7 +62,7 @@ void GtuVirtualOperatingSystem::MailBox::qt_static_metacall(QObject *_o, QMetaOb
 
 const QMetaObject GtuVirtualOperatingSystem::MailBox::staticMetaObject = {
     { &QWidget::staticMetaObject, qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox.data,
-      qt_meta_data_GtuVirtualOperatingSystem__MailBox,  qt_static_metacall, 0, 0}
+      qt_meta_data_GtuVirtualOperatingSystem__MailBox,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
@@ -72,8 +73,8 @@ const QMetaObject *GtuVirtualOperatingSystem::MailBox::metaObject() const
 
 void *GtuVirtualOperatingSystem::MailBox::qt_metacast(const char *_clname)
 {
-    if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox.stringdata))
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_GtuVirtualOperatingSystem__MailBox.stringdata0))
         return static_cast<void*>(const_cast< MailBox*>(this));
     return QWidget::qt_metacast(_clname);
 }
--- moc_VirtualOperatingSystemForm.cpp
+++ moc_VirtualOperatingSystemForm.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'VirtualOperatingSystemForm.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'VirtualOperatingSystemForm.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.2.1. It"
+#error "This file was generated using the moc from 5.5.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,18 +20,19 @@
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm_t {
     QByteArrayData data[1];
-    char stringdata[55];
+    char stringdata0[54];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData) \
+    qptrdiff(offsetof(qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
     )
 static const qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm_t qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm = {
     {
-QT_MOC_LITERAL(0, 0, 53)
+QT_MOC_LITERAL(0, 0, 53) // "GtuVirtualOperatingSystem::Vi..."
+
     },
-    "GtuVirtualOperatingSystem::VirtualOperatingSystemForm\0"
+    "GtuVirtualOperatingSystem::VirtualOperatingSystemForm"
 };
 #undef QT_MOC_LITERAL
 
@@ -61,7 +62,7 @@ void GtuVirtualOperatingSystem::VirtualOperatingSystemForm::qt_static_metacall(Q
 
 const QMetaObject GtuVirtualOperatingSystem::VirtualOperatingSystemForm::staticMetaObject = {
     { &QMainWindow::staticMetaObject, qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm.data,
-      qt_meta_data_GtuVirtualOperatingSystem__VirtualOperatingSystemForm,  qt_static_metacall, 0, 0}
+      qt_meta_data_GtuVirtualOperatingSystem__VirtualOperatingSystemForm,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
@@ -72,8 +73,8 @@ const QMetaObject *GtuVirtualOperatingSystem::VirtualOperatingSystemForm::metaOb
 
 void *GtuVirtualOperatingSystem::VirtualOperatingSystemForm::qt_metacast(const char *_clname)
 {
-    if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm.stringdata))
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_GtuVirtualOperatingSystem__VirtualOperatingSystemForm.stringdata0))
         return static_cast<void*>(const_cast< VirtualOperatingSystemForm*>(this));
     return QMainWindow::qt_metacast(_clname);
 }
